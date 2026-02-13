98e7bff6221fbb6c87e4831e38676cce680b3822
vishwajeet <vishwajeet17raj@gmail.com>
2026-02-13 09:51:07 +0530

    Age Added

--- index.cs
+++ index.cs
@@ -3,12 +3,8 @@ using System;
 public class Program{
     public static void Main(){
         string name = "Vishwajeet";
-<<<<<<< HEAD
         Console.WriteLine("Hello, " + name + "!");
-=======
         int Age = 20;
-        Console.WriteLine("Hello, " + name + "!");
         Console.WriteLine("You are " + Age + " years old.");
->>>>>>> Dev1
     }
 }
