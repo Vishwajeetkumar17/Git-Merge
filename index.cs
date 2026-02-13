818aa9dae27c9c25dbcb6ccf102a8e2d873a42cc
vishwajeet <vishwajeet17raj@gmail.com>
2026-02-13 09:56:00 +0530

    Conflict removed

--- index.cs
+++ index.cs
@@ -3,8 +3,8 @@ using System;
 public class Program{
     public static void Main(){
         string name = "Vishwajeet";
+        int Age = 25;
         Console.WriteLine("Hello, " + name + "!");
-        int Age = 20;
         Console.WriteLine("You are " + Age + " years old.");
     }
 }
